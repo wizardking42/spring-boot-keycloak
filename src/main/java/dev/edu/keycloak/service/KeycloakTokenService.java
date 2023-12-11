4f1d90146f9a787f6fc47c8a57d0aba6b0c789f0
edneyrafael <edneyrafael@hotmail.com>
2023-12-11 20:05:11 +0100

    spring boot keycloak setup

--- src/main/java/dev/edu/keycloak/service/KeycloakTokenService.java
+++ src/main/java/dev/edu/keycloak/service/KeycloakTokenService.java
@@ -1,6 +1,7 @@
 package dev.edu.keycloak.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,10 +10,19 @@ import reactor.core.publisher.Mono;
 public class KeycloakTokenService
 {
     @Value("${keycloak.server-url}")
-    String serverUrl;
+    private String serverUrl;
     @Value("${keycloak.realm}")
-    String realm;
-    public Mono<String> getAccessToken()
+    private String realm;
+    @Value("${keycloak.client-id}")
+    private String clientId;
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+    @Value("${keycloak.admin-username}")
+    private String admin_username;
+    @Value("${keycloak.admin-password}")
+    private String admin_password;
+
+    public Mono<String> getAdminAccessToken()
     {
         WebClient webClient = WebClient.create();
         String url = "%s/realms/$s/protocol/openid-connect/token".formatted(serverUrl, realm);
@@ -20,8 +30,14 @@ public class KeycloakTokenService
                 .uri(url)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .bodyValue(
-                        "client_id="
-                );
-
+                                "client_id=$s&" +
+                                "client_secret=$s&" +
+                                "grant_type=password&" +
+                                "username=$s&" +
+                                "password=$s&" +
+                                "scope=openid".formatted(clientId, clientSecret, admin_username, admin_password)
+                )
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
